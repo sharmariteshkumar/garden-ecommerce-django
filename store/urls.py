fe9416482ea5595e0e630e43d20843dba122ab05
sharmariteshkumar <sharmaritesh4943@gmail.com>
2026-09-05 01:13:06 +0530

    Card view all products

--- store/urls.py
+++ store/urls.py
@@ -1,35 +1,29 @@
 from django.urls import path
-
 from . import views
-
 
 urlpatterns = [
     path("", views.home, name="store_home"),
-
+    path("products/", views.all_products, name="all_products"),  
     path(
         "product/<slug:slug>/",
         views.product_detail,
         name="product_detail"
     ),
-
     path(
         "cart/",
         views.cart,
         name="cart"
     ),
-
     path(
         "cart/add/<int:product_id>/",
         views.add_to_cart,
         name="add_to_cart"
     ),
-
     path(
         "cart/update/<int:product_id>/",
         views.update_cart,
         name="update_cart"
     ),
-
     path(
         "cart/remove/<int:product_id>/",
         views.remove_from_cart,
