92c9eec0d60fa5b74ada88d826dab81a7ee10c6b
sharmariteshkumar <sharmaritesh4943@gmail.com>
2026-09-05 15:58:19 +0530

    Connect Render PostgreSQL Database

--- myproject/settings.py
+++ myproject/settings.py
@@ -91,7 +91,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        default=os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
