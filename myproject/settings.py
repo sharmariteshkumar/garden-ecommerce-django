e5c6e340ec997a26c46d3d85ae0470c17ce3bbef
sharmariteshkumar <sharmaritesh4943@gmail.com>
2026-09-07 12:33:33 +0530

    Configure PostgreSQL and Cloudinary

--- myproject/settings.py
+++ myproject/settings.py
@@ -92,8 +92,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
         conn_health_checks=True,
     )
