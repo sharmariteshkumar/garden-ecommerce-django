997d9859577075aba4ee69a817e3fd2abbb34762
sharmariteshkumar <sharmaritesh4943@gmail.com>
2026-09-10 15:33:08 +0530

    Add customer profile and my orders

--- store/urls.py
+++ store/urls.py
@@ -20,6 +20,5 @@ urlpatterns = [
     path("payment-success/",views.payment_success,name="payment_success"),
     path("order-success/<int:order_id>/",views.order_success,name="order_success"),
     path("payment-failed/",views.payment_failed,name="payment_failed"),
-    path("profile/", views.profile, name="profile"),
     path("my-orders/", views.my_orders, name="my_orders"),
 ]
